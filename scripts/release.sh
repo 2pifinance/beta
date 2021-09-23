d5ac9c921793657bb872856e1b3503593f66f911
Batudo <batudo@2pi.network>
2021-09-23 19:17:25 -0300

    With release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,6 @@ set -eu
 
 NEXT_PUBLIC_SENTRY_RELEASE=${NEXT_PUBLIC_SENTRY_RELEASE:=CF-${CF_PAGES_COMMIT_SHA:=dev}}
 
-echo "Building release $NEXT_PUBLIC_SENTRY_RELEASE"
+export NEXT_PUBLIC_SENTRY_RELEASE
 
-next build && next export
+node scripts/sentry.js
