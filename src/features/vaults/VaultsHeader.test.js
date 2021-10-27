732aaaefce52c9296851cbad85a75301fea0a1ac
Batudo <batudo@2pi.network>
2021-10-27 03:00:17 -0300

    Fix VaultsHeader test

--- src/features/vaults/VaultsHeader.test.js
+++ src/features/vaults/VaultsHeader.test.js
@@ -1,6 +1,10 @@
 import BigNumber from 'bignumber.js'
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 import VaultsHeader from './VaultsHeader'
+
+const mockStore = configureStore([])
 
 const vaults = [
   { deposited: new BigNumber(10), tvl: new BigNumber(20), price: new BigNumber(1) },
@@ -9,10 +13,24 @@ const vaults = [
 ]
 
 describe('VaultsHeader component', () => {
+  let store
+
+  beforeEach(() => {
+    const initialState = {
+      wallet: {}
+    }
+
+    store = mockStore(initialState)
+  })
+
   test('VaultsHeader component renders', () => {
     const props = { vaults, chainId: 137, connected: true }
 
-    render(<VaultsHeader {...props} />)
+    render(
+      <Provider store={store}>
+        <VaultsHeader {...props} />
+      </Provider>
+    )
 
     const elements = screen.getAllByText(/Polygon/)
 
