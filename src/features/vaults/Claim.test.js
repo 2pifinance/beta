cce9dd0b36fcf764c4f150c1ada4d03cdee4a0f2
Matías Galí <matiasxgali@gmail.com>
2021-11-12 14:17:12 -0300

    Add vaults factory.

--- src/features/vaults/Claim.test.js
+++ src/features/vaults/Claim.test.js
@@ -1,30 +1,12 @@
-import BigNumber from 'bignumber.js'
 import { render, screen } from '@testing-library/react'
+import { buildVault } from '../../../test/support/vaultsFactory'
 import { Provider } from '../../store'
 import Claim from './Claim'
 
-const vault = {
-  id:             'polygon-dai-aave',
-  chainId:        137,
-  symbol:         'DAI',
-  token:          'dai',
-  tokenDecimals:  new BigNumber(18),
-  earn:           'DAI',
-  uses:           'Aave',
-  apy:            new BigNumber(0.17),
-  price:          new BigNumber(1),
-  tvl:            new BigNumber(1000),
-  withdrawalFee:  new BigNumber(10),
-  allowance:      new BigNumber(0),
-  balance:        new BigNumber(0),
-  deposited:      new BigNumber(0),
-  sharesDecimals: new BigNumber(18),
-  sharePrice:     new BigNumber(1.5),
-  twoPiEarned:    new BigNumber(10)
-}
-
 describe('Claim component', () => {
   test('Claim component renders', () => {
+    const vault = buildVault()
+
     render(
       <Provider>
         <Claim vault={vault} />
---
+++ test/support/vaultsFactory.js
@@ -0,0 +1,37 @@
+import BigNumber from 'bignumber.js'
+
+export const buildVault = (data = {}) => {
+  return {
+    // Static data
+    id:             'polygon-dai-aave',
+    chainId:        137,
+    address:        '0x3B353b1CBDDA3A3D648af9825Ee34d9CA816FD38',
+    symbol:         'DAI',
+    token:          'dai',
+    earn:           'DAI',
+    uses:           'Aave',
+
+    // Vault data
+    apy:            0.17,
+    vaultApy:       0.17,
+    rewardsApr:     0,
+    withdrawalFee:  new BigNumber(10),
+    tokenDecimals:  new BigNumber(18),
+    price:          new BigNumber(1),
+    quota:          new BigNumber(9999),
+    availableQuota: new BigNumber(8999),
+    tvl:            new BigNumber(1000),
+    isPaused:       false,
+
+    // Wallet data
+    allowance:      new BigNumber(0),
+    balance:        new BigNumber(0),
+    deposited:      new BigNumber(0),
+    sharesDecimals: new BigNumber(18),
+    sharePrice:     new BigNumber(1.5),
+    twoPiEarned:    new BigNumber(10),
+
+    // Overwrites
+    ...data,
+  }
+}
