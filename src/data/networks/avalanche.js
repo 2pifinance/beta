98355853d56effd729431f2c5156aa51501ef242
Matías Galí <matiasxgali@gmail.com>
2021-11-25 15:39:51 -0300

    Add Avalanche mainnet.

--- src/data/networks/avalanche.js
+++ src/data/networks/avalanche.js
@@ -2,6 +2,18 @@
 // Avalanche
 //
 
+export const avalancheMainnet = {
+  chainId:   `0x${parseInt(43114).toString(16)}`,
+  chainName: 'Avalanche Mainnet',
+  nativeCurrency: {
+    name:     'AVAX',
+    symbol:   'AVAX',
+    decimals: 18,
+  },
+  rpcUrls:           [ 'https://api.avax.network/ext/bc/C/rpc/' ],
+  blockExplorerUrls: [ 'https://snowtrace.io/' ]
+}
+
 export const avalancheTestnet = {
   chainId:   `0x${parseInt(43113).toString(16)}`,
   chainName: 'Avalanche Testnet (Fuji)',
